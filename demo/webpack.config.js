90fd05517e44d2f1dfdb764fb5697ab9503318c7
claytongulick <claytongulick@gmail.com>
2019-04-12 16:12:51 -0500

    added dist, work on npm scripts

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,16 +1,32 @@
 let path = require('path');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
 
 let config = {
-    entry: './app.js',
-    mode: "development",
-    devtool: 'inline-source-map',
-    devServer: {
-        publicPath: '/',
-        overlay: {
-            warnings: true,
-            errors: true
-        }
-  }
+  context: __dirname,
+  entry: './app.js',
+  output: {
+    path: __dirname + '/dist'
+  },
+  mode: "development",
+  devtool: 'inline-source-map',
+  devServer: {
+    publicPath: '/',
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'index.html'),
+        to: path.resolve(__dirname, 'dist')
+      },
+    ]
+    ),
+  ]
 };
 
 module.exports = config;
